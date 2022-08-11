471e23b1f4f946e2dba342d38004f71543ae5c68
Sai Tanusha <60430970+taffy5@users.noreply.github.com>
2022-08-11 15:00:13 +0530

    Update play.cpp

--- Lecture025 Intro to Pointers in C++/play.cpp
+++ Lecture025 Intro to Pointers in C++/play.cpp
@@ -8,30 +8,30 @@ int main () {
 
     //cout << *p << endl;
 
-    /*
+    // First way to declare a pointer
     int i = 5;
-
     int *q = &i;
     cout << q << endl;
     cout << *q << endl;
 
-    int *p = 0;
+    // Second way to declare a pointer
+    int *p = 0; // this we wrote first to initialise a pointer to 0 
     p = &i;
-
-    cout << p << endl;
-    cout << *p << endl;
-    */
+    cout << p << endl; // this will print out address "0x16f933488"
+    cout << *p << endl; // this will print out value 5
 
    int num = 5;
    int a = num;
    cout << "a before " << num << endl;
    a++;
+   // this will increment the address of a
    cout << "a after " << num << endl;
 
 
    int *p  = &num;
    cout << "before " << num << endl;
    (*p)++;
+   // this will increment the value num from 5 to 6
    cout << "after " << num << endl;
 
     //copying a pointer
@@ -45,6 +45,7 @@ int main () {
    //cout <<  (*t)++ << endl;
    *t = *t +1;
     cout << *t << endl;
+    // will increment 3 to 4
     cout << " before t " << t << endl;
     t = t + 1;
     cout << " after t " << t << endl;
