a8a3c72fd9944fa535b6130f7111c8ccf9105a31
Sai Tanusha <60430970+taffy5@users.noreply.github.com>
2022-08-12 08:19:19 +0530

    Update pointer_characterArray.cpp

--- Lecture026 Pointers with Arrays and Functions/pointer_characterArray.cpp
+++ Lecture026 Pointers with Arrays and Functions/pointer_characterArray.cpp
@@ -1,3 +1,5 @@
+// ***** CHARACTER ARRAY ***** 
+
 #include<iostream>
 using namespace std;
 
@@ -5,11 +7,12 @@ int main() {
 
     int arr[5] = {1,2,3,4,5};
     char ch[6] = "abcde";
+    // we cant write here ch[5] as when we put a string in ch array then it will always end with null character
 
-
-    cout << arr << endl;
+// means ki int array or ch array ka cout implementation different hain
+    cout << arr << endl;         // some address got printed
     //attention here
-    cout << ch << endl;
+    cout << ch << endl;         // "abcde" got printed 
     
     char *c = &ch[0];
     //prints entire string
@@ -19,6 +22,26 @@ int main() {
     char *p = &temp;
 
     cout << p << endl;
+    
+    // it outputs value "z00j"
+    // explanation :- pehle vo z print karega aur vo tab tak print karte rahega jab tak usko ek null character nahi mil jaata.
+    // toh iss case mein usko j ke baad null character mila isliye uske baad stop hogaya
+    
+    /*   BUT VERY RISKY SO DONT DO LIKE THIS 
+    
+    char *c = "abcde";
+    
+    Behind the scenes
+        Its a 2 step process:-
+            1. temporary memory mein initialised value rakh lo
+            2. aur second free memory hain usmein usko copy kardo
+            
+            
+     so we only use :-
+     
+     char *c = "abcde";
+     
+    */
 
 
     return 0;
