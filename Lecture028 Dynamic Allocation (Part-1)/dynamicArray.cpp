d7fe9a1a2b64d7453af2816460791e2e1dfdcdac
Sai Tanusha <60430970+taffy5@users.noreply.github.com>
2022-08-16 09:46:49 +0530

    Update dynamicArray.cpp

--- Lecture028 Dynamic Allocation (Part-1)/dynamicArray.cpp
+++ Lecture028 Dynamic Allocation (Part-1)/dynamicArray.cpp
@@ -15,10 +15,15 @@ int main() {
     int n;
     cin >> n;
     
-    //variable size array
+    // before " int arr[n]; "  we used to say is a bad practice as 
+    // size jo hain run time mein already pata hona makes it very difficult
+    // rather than compile time mein pata hona
+    // when we use n as the size variable
+    
+    //variable size array ka array create kar liya hain
     int* arr = new int[n];
 
-    //takign inputn in aray
+    //taking input n in aray
     for(int i=0; i<n; i++) {
         cin >> arr[i];
     }
@@ -27,17 +32,25 @@ int main() {
 
     cout << "answer is "<< ans << endl;
 
-//case 1
+// these while loop cases are just for the system to crash :)
+    
+    //case 1     - STATIC WAY to crash a system
     while(true) {
         int i = 5;
     }
     
-//case 2
+//case 2        - DYNAMMIC WAY to crash a system
     while(true) {
         int* ptr = new int;
     }
 
-
+ char ch= 'q';
+    cout<< sizeof(ch)<< endl;    // 1 byte   (ch hain isliye)
+    
+char *c = &ch;
+    cout<< sizeof(c) << endl;   // 9 byte    (ch hain isliye)
+    
+  //  int ke case mein 12 byte uthayega vo. 4 byte or 8 byte
 
 
     return 0;
