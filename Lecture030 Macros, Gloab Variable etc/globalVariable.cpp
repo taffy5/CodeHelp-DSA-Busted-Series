5ce12535a3910e9107b34b841e089ede0fd02d0e
Sai Tanusha <60430970+taffy5@users.noreply.github.com>
2022-08-18 18:35:40 +0530

    Update globalVariable.cpp

--- Lecture030 Macros, Gloab Variable etc/globalVariable.cpp
+++ Lecture030 Macros, Gloab Variable etc/globalVariable.cpp
@@ -1,18 +1,18 @@
 #include<iostream>
 using namespace std;
 
-int score = 15;
+int score = 15;             // "score" is the share variable or GLOBAL VARIABLE
 
 
-void a(int& i) {
+void a(int& i) {                      // &i is the reference variable
 
     cout << score << " in a" << endl;
-    score++;
+   // score++;
     char ch = 'a';
     cout << i << endl;
 }
 
-void b(int& i) {
+void b(int& i) {                      // &i is the reference variable
     cout << score << " in b" << endl;
     cout << i << endl;
 }
@@ -25,3 +25,11 @@ int main() {
 
     return 0;
 }
+
+// Using global variable is a very bad practice as it can be changed by anyone in any of the function calls
+// Lets suppose in 
+// void a - we add - score ++ ;
+// then in void b 
+// we get the incremented value of the score which is wrong
+
+// So better to use REFERENCE VARIABLE rather than GLOBAL VARIABLE
