86c28e2fb360aeeeda62266751077789969b3bf4
Sai Tanusha <60430970+taffy5@users.noreply.github.com>
2022-08-18 18:21:55 +0530

    Update macros.cpp

--- Lecture030 Macros, Gloab Variable etc/macros.cpp
+++ Lecture030 Macros, Gloab Variable etc/macros.cpp
@@ -7,6 +7,12 @@ int main() {
 
     int r = 5;
     //double pi = 3.14;
+    
+    // if we use the number 9 line instead of 4th line then we can later add that
+    // PI = PI + 1;
+    
+    // but after introduction of line 4 we cannot increment or decrement that particular variable
+    // for which macro is defined. 
 
     double area = PI * r * r;
 
