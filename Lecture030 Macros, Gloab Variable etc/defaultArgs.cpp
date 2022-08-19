6715b506023d003748d5dc2188c2b45465c89776
Sai Tanusha <60430970+taffy5@users.noreply.github.com>
2022-08-19 19:27:05 +0530

    Update defaultArgs.cpp

--- Lecture030 Macros, Gloab Variable etc/defaultArgs.cpp
+++ Lecture030 Macros, Gloab Variable etc/defaultArgs.cpp
@@ -14,10 +14,18 @@ int main() {
     int arr[5] = {1,4,7,8,9};
     int size = 5;
 
-    print(arr, size);
+    print(arr, size); //jab start =0 de diya value toh bhi no problem
     cout << endl;
-    print(arr, size, 2);
+    print(arr, size, 2); // phirse call pe bhi no problem
 
 
     return 0;
 }
+
+// condition :- 
+// default argument hamesha right parameter se start karna chahiye matlab
+// first ab start
+// then int n
+// then arr
+
+// beech se hi agar int n pe kardein toh error dikha dega
